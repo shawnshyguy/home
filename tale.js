78f6593ba3c433d333e5bc1c767151fdf11139ef
Shawn <shawnshyguy@mail.com>
2016-08-11 23:31:18 -0400

    Create tale.js

---
+++ tale.js
@@ -0,0 +1,57 @@
+
+var quotes=new Array()
+
+//change the quotes if desired. Add/ delete additional quotes as desired.
+
+quotes[0]='Dialing...'
+
+quotes[1]='Thank you Mario,<br>but good content is in another website!'
+
+quotes[2]='You are now entering the Gungeon.'
+
+quotes[3]='Don\'t forget to like, favorite, and click that subscribe button to join the ShyGuy Brethrin'
+
+quotes[4]='Read the description, please.'
+
+quotes[5]='PRANK [IN THE HOOD]'
+
+quotes[6]='PRANK [GONE VIOLENT]'
+
+quotes[7]='PRANK [GONE SEXUAL]'
+
+quotes[8]='All of the uploads are willingly submitted by a small team of content creators. Also, please keep your requests to a minimum.'
+
+quotes[9]='If you see any inspirational quotes when you refresh, I forgot to cut some lines out. Oops.'
+
+quotes[10]='Number of days without a roadblock in the CSS: [000]'
+
+quotes[11]='THE MOST INSANE PERSON ON YOUTUBE IN THE UNIVERSE IN EXISTENCE EVER LITERALLY'
+
+quotes[12]='Let\'s, get roiight, into the neeeews!'
+
+quotes[13]='Always backslash before you apostrophe'
+
+quotes[14]='The FitnessGram Pacer Test'
+
+quotes[15]='Grand Dad? Fleenstones?! What the sh'
+
+quotes[16]='Well, the years start comin\' and they don\'t stop comin\''
+
+quotes[17]='It\'s a conspiracy.'
+
+quotes[18]='You cannot give up just yet...<br>PERSON! Stay determined...'
+
+quotes[19]='The blue line train to Flavortown is now approaching.'
+
+quotes[20]='The blue line train to Flavortown is now arriving.'
+
+quotes[21]='I don\'t have a witty saying to put here. Welcome to my site.'
+
+quotes[22]='Sample text'
+
+quotes[23]='<i>write text for quote 23 here</i>'
+
+quotes[24]='Outlet. Not a <i>plughole</i>'
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
