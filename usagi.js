8a11158ad053813d01515677ed88de1b92e9a1d4
Shawn <shawnshyguy@mail.com>
2017-05-17 23:10:53 -0400

    Create usagi.js

---
+++ usagi.js
@@ -0,0 +1,70 @@
+
+var quotes=new Array()
+
+//change the quotes if desired. Add/ delete additional quotes as desired.
+
+quotes[0]='https://68.media.tumblr.com/44be7556d3c9546bf3f52cf44f09ab40/tumblr_inline_nzlalo919c1r5m5yo_500.gif'
+
+quotes[1]='http://24.media.tumblr.com/2f45b959a06926675f90e2d23f5335bc/tumblr_mxoyw0qn2x1t5cdlzo1_500.gif'
+
+quotes[2]='http://38.media.tumblr.com/41d94519e1bccc3dd8de334f7fcd6f37/tumblr_nfwdbtcetk1r23i51o1_500.gif'
+
+quotes[3]='http://vignette2.wikia.nocookie.net/sailormoon/images/7/7e/Usagi_crying.gif.gif'
+
+quotes[4]='Read the description, please.'
+
+quotes[5]='PRANK [IN THE HOOD]'
+
+quotes[6]='PRANK [GONE VIOLENT]'
+
+quotes[7]='PRANK [GONE SEXUAL]'
+
+quotes[8]='All of the uploads are willingly submitted by a small team of content creators. Also, please keep your requests to a minimum.'
+
+quotes[9]='If you see any inspirational quotes when you refresh, I forgot to cut some lines out. Oops.'
+
+quotes[10]='Number of days without a roadblock in the CSS: [000]'
+
+quotes[11]='THE MOST INSANE PERSON ON YOUTUBE IN THE UNIVERSE IN EXISTENCE EVER LITERALLY'
+
+quotes[12]='Let\'s, get roiight, into the neeeews!'
+
+quotes[13]='Always backslash before you apostrophe'
+
+quotes[14]='The FitnessGram Pacer Test'
+
+quotes[15]='Grand Dad? Fleenstones?! What the sh'
+
+quotes[16]='Well, the years start comin\' and they don\'t stop comin\''
+
+quotes[17]='It\'s a conspiracy.'
+
+quotes[18]='You cannot give up just yet...<br>PERSON! Stay determined...'
+
+quotes[19]='The blue line train to Flavortown is now approaching.'
+
+quotes[20]='The blue line train to Flavortown is now arriving.'
+
+quotes[21]='I don\'t have a witty saying to put here. Welcome to my site.'
+
+quotes[22]='Sample text'
+
+quotes[23]='<i>write text for quote 23 here</i>'
+
+quotes[24]='Outlet. Not a <i>plughole</i>'
+
+quotes[25]='THE BONE ZONE'
+
+quotes[26]='😂 😂 WHO DID THIS 😂 😂'
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
+//feel free to steal this script, I did just that.
+//Well, it was open source...
+//But regardless, use this stuff if you want.
+//hell, you went sifting through my github, so this is my treat to you
+//you don't like my gift?
+//Fine then!
+//you are now BLOCKED from Twitch, Twitter, YouTube... nevermind, i need the views
+//please dont leave
+//take the script, man, just dont hurt me!
